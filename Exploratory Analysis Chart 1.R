e691d14eebf89add04cc8c97a118e3a454d99d06
eykh5 <eykh5@uw.edu>
2022-02-19 17:54:28 -0800

    made changes to chart 1

--- Exploratory Analysis Chart 1.R
+++ Exploratory Analysis Chart 1.R
@@ -5,8 +5,8 @@ library(ggplot2)
 # This line plot visualization displays the difference in profit for buying stock from Apple at day's open and selling it at day's close. It's purpose is to gain insight on whether this way of day trading is profitable over time.
 
 ## Describe any notable observations and insights from the chart
-# This chart reveals an overall increase in the difference in profit while day-trading Apple stocks over time. While there is a significant increase in profits some days, there is similar decrease in profits on other days. Day-trading this way with Apple stock is relatively risky due to the significant increase and decrease in profit in recent times. 
-
+# This chart reveals an overall increase in the difference in profit while day-trading Apple stocks over time. While there is a significant increase in profits some days, there is similar decrease in profits on other days. Day-trading this way with Apple stock is relatively risky due to the significant increase and decrease in profit in recent times.
+AAPL <- read.csv("~/data_files/AAPL.csv")
 AAPL <- AAPL %>% 
   group_by(Open, Close) %>% 
   mutate(buy_opening_sell_closing = c(Open - Close))
