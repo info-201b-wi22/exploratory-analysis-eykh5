4f7213f3182c5b8ffe528f0fc7a3f97e4e23ae49
eykh5 <eykh5@uw.edu>
2022-02-20 10:00:58 -0800

    calculated Values done

---
+++ Exploratory Analysis Calculated Values.R
@@ -0,0 +1,22 @@
+chart1 <-source("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-eykh5/main/Exploratory%20Analysis%20Chart%201.R?token=GHSAT0AAAAAABRCCMDVDJWUQ2J4AG7EUER4YQ3W7UA")
+chart2 <-source("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-eykh5/main/Exploratory%20Analysis%20Chart%202.R?token=GHSAT0AAAAAABRCCMDUVCIPDAYZNISGLBQ6YQ3XAGA")
+chart3 <-source("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-eykh5/main/Exploratory%20Analysis%20Chart%203.R?token=GHSAT0AAAAAABRCCMDU7RFY45IUQ5S3KWUOYQ3XAQA")
+
+library(dplyr)
+
+rm(list=ls())
+
+AAPL_chart1 <- chart1$value$data
+
+AAPL_chart2 <- chart2$value$data
+
+AAPL_chart3 <- chart3$value$data
+
+profit_chart_1 <- AAPL_chart1 %>% filter(Date==max(Date)) %>% pull(cumulative_profit)
+
+profit_chart_2<- AAPL_chart2 %>% filter(Date==max(Date)) %>% pull(cumulative_profit)
+
+profit_buy_day_one <- AAPL_chart3 %>% filter(Date==max(Date)) %>% pull(all_one_day_cumulative_profit)/nrow(AAPL_chart3)
+
+profit_buy_everyday <- AAPL_chart3 %>% filter(Date==max(Date)) %>% pull(cumulative_profit)/nrow(AAPL_chart3)
+
--- Exploratory Analysis Chart 3.R
+++ Exploratory Analysis Chart 3.R
@@ -11,26 +11,5 @@ AAPL <- AAPL %>% mutate(cumulative_profit = Close*day_number - cumulative_price)
 
 AAPL <- AAPL %>% mutate(total_price = AAPL[1,]$Open * nrow(AAPL)) %>% mutate(all_one_day_cumulative_profit = Close*nrow(AAPL) - total_price)
 
-ggplot(data = AAPL) + geom_point(mapping = aes(x = as.Date(Date), y = cumulative_profit)) + geom_point(mapping = aes(x = as.Date(Date), y = all_one_day_cumulative_profit)) + 
-  labs(x = "Date", y = "Cumulative Profit of buying at Closing, selling at Opening", title = "Buying at Closing and Selling at Opening")
-
-
-# day_one_open <- AAPL[1,]$Open
-# 
-# open_price <- AAPL$Open
-# 
-# cumulative_sum <- c(0)
-# 
-# for (i in 1:length(open_price)){
-#   sum <- sum(open_price[1:i])
-#   cumulative_sum <- append(cumulative_sum, sum)
-# }
-# 
-# cumulative_sum <- cumulative_sum[-1]
-# 
-# AAPL <- mutate(AAPL, all_in_day_one_cost = 1826*day_one_open)
-# 
-# AAPL <- mutate(AAPL, buy_one_a_day_cost = cumulative_sum)
-# 
-# strat_all_buy<- ggplot(data = AAPL, aes(x=as.Date(Date))) + geom_point(mapping = aes(y = all_in_day_one_cost)) + geom_point(mapping = aes(y=buy_one_a_day_cost))
-# strat_all_buy
+ggplot(data = AAPL) + geom_point(mapping = aes(x = as.Date(Date), y = cumulative_profit)) + geom_point(mapping = aes(x = as.Date(Date), y = all_one_day_cumulative_profit, color = all_one_day_cumulative_profit)) + 
+  labs(x = "Date", y = "Cumulative Profit of buying at Closing, selling at Opening", color = "Profit($)", title = "Profit made from $40,000 Investment in Apple")
