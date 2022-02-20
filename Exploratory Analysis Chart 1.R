cbd9f18d6c3067bb88820f29b98bb4929864ece7
eykh5 <eykh5@uw.edu>
2022-02-19 18:02:00 -0800

    fixed some issues

--- Exploratory Analysis Chart 1.R
+++ Exploratory Analysis Chart 1.R
@@ -6,7 +6,7 @@ library(ggplot2)
 
 ## Describe any notable observations and insights from the chart
 # This chart reveals an overall increase in the difference in profit while day-trading Apple stocks over time. While there is a significant increase in profits some days, there is similar decrease in profits on other days. Day-trading this way with Apple stock is relatively risky due to the significant increase and decrease in profit in recent times.
-AAPL <- read.csv("~/data_files/AAPL.csv")
+AAPL <- read.csv("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-eykh5/main/data_files/AAPL.csv?token=GHSAT0AAAAAABRCCMDV75U72KWW2PYCKDHIYQ23NNQ")
 AAPL <- AAPL %>% 
   group_by(Open, Close) %>% 
   mutate(buy_opening_sell_closing = c(Open - Close))
